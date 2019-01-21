8198f144801f8c8b0a55898aebea4d90229fb20d
Michael Thorne <michael.thorne@gmail.com>
2019-01-21 07:35:27 +0200

    WIP: Assignment 2

---
+++ assignment-2/src/Char/Char.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Char.css'
+
+const char = (props) => {
+  return (
+    <div className="Char" onClick={props.clicked}>{props.character}</div>
+  )
+}
+
+export default char
