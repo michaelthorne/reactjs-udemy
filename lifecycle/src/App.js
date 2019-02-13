20d4fa4545f3857439a95f0260b646cd125ca495
Michael Thorne <michael.thorne@gmail.com>
2019-02-13 07:05:37 +0200

    React lifecycle hooks test

---
+++ lifecycle/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+
+  state = {}
+
+  static getDerivedStateFromProps (props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  componentDidMount () {
+    console.log('[App.js] componentDidMount')
+  }
+
+  render () {
+    console.log('[App.js] render')
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.props.title}</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
