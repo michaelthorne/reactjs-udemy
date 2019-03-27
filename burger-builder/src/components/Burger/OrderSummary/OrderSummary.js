a6f5ccec82647b629847e18c5200218b4d42569b
Michael Thorne <michael.thorne@gmail.com>
2019-03-27 18:42:06 +0200

    Reactored layout

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(ingredientKey => {
-    return <li key={ingredientKey}>
-      <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {props.ingredients[ingredientKey]}
-    </li>
-  })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button buttonType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
+class OrderSummary extends Component {
+  componentWillUpdate (nextProps, nextState, nextContext) {
+    // Not updated unless the modal is shown
+    console.log('[OrderSummary] WillUpdate')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(ingredientKey => {
+      return <li key={ingredientKey}>
+        <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
+      </li>
+    })
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button buttonType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Modal.module.scss'
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        opacity: props.show ? '1' : '0',
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-)
+class Modal extends Component {
+  componentWillUpdate (nextProps, nextState, nextContext) {
+    console.log('[Modal] WillUpdate')
+  }
 
-export default modal
+  shouldComponentUpdate (nextProps, nextState, nextContext) {
+    // Only render the order summary if the modal is shown
+    return nextProps.show !== this.props.show
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            opacity: this.props.show ? '1' : '0',
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal
