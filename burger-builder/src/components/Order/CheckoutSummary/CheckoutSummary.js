135472b86fb0c085d5e61faef9c049f14ad91783
Michael Thorne <michael.thorne@gmail.com>
2019-04-23 08:51:02 +0200

    WIP: adding routing

---
+++ burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
+
+import classes from './CheckoutSummary.module.css'
+
+const checkoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>Hope it tastes good!</h1>
+      <div style={{
+        margin: 'auto',
+        width: '100%'
+      }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button
+        btnType="Danger"
+        clicked=""
+      >Cancel</Button>
+      <Button
+        btnType="Success"
+        clicked=""
+      >Continue</Button>
+    </div>
+  )
+}
+
+export default checkoutSummary
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,29 +94,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true })
-
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Michael Thorne',
-        address: {
-          street: '123 Street, Cape Town',
-          zipcode: '123123',
-          country: 'South Africa'
-        },
-        email: 'test@email.com',
-        deliveryMethod: 'collect'
-      }
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false })
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      })
+    // this.setState({ loading: true })
+    //
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Michael Thorne',
+    //     address: {
+    //       street: '123 Street, Cape Town',
+    //       zipcode: '123123',
+    //       country: 'South Africa'
+    //     },
+    //     email: 'test@email.com',
+    //     deliveryMethod: 'collect'
+    //   }
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false })
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false })
+    //   })
+    this.props.history.push('/checkout')
   }
 
   render () {
